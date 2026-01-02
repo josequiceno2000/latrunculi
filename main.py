876da2dc3a9c252f35c0788240a0761a53474f26
Jose Quiceno <josequiceno000@gmail.com>
2026-01-01 21:32:32 -0600

    feat(main): allow movement via keypresses

--- main.py
+++ main.py
@@ -4,16 +4,31 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() // 2, screen.get_height() // 2)
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((250, 250, 250))
-    
+    screen.fill(("darkblue"))
+
+    pygame.draw.circle(screen, "yellow", player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt  
 
     pygame.display.flip()
-    clock.tick(60)
+    
+    dt = clock.tick(60) / 1000
 
 pygame.quit()
